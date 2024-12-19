62d0a5240463c46838c76b21306923230421ebf8
Aaron Hooper <aaron@fxdigital.uk>
2024-12-19 07:32:11 +0000

    change fps to 120

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
-#define FRAMES_PER_SEC 60
+#define FRAMES_PER_SEC 120
 #define MAX_WORD_LENGTH 20
 
 static const char *WINDOW_TITLE = "typespeed";
