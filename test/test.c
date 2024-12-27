825b747f0170de59c3c2c1e5048dd92c9a828904
Aaron Hooper <aaron@fxdigital.uk>
2024-12-27 02:00:04 +0000

    enable testing via cmake and add arena test

---
+++ test/test.c
@@ -0,0 +1,21 @@
+#include "arena.h"
+#include "munit/munit.h"
+
+void test_pointers_exist_after_realloc() {
+  Arena arena = {0};
+  arena_init(&arena, 16);
+  int *ptrs[16];
+
+  for (int i = 0; i < 16; ++i) {
+    ptrs[i] = arena_alloc(&arena, sizeof(int));
+    *ptrs[i] = i;
+  }
+
+  arena_alloc(&arena, 1024 * 1024);
+
+  for (int i = 0; i < 16; ++i) {
+    munit_assert_int(*ptrs[i], ==, i);
+  }
+}
+
+int main(void) { test_pointers_exist_after_realloc(); }
